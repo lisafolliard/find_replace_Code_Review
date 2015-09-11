64e6d10539104c0783805c219f0af8848a72d928
Lisa Folliard <lisaf37@gmail.com>
2015-09-11 10:48:12 -0700

    add user interface logic to include index.html form and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 var findReplace = function(sentence, word, replacement) {
     return sentence.replace(word, replacement);
 };
+
+$(document).ready(function() {
+  $("form#find_replace").submit(function(event) {
+    var sentence = ($("input#sentence").val());
+    var word = ($("input#word").val());
+    var replacement = ($("input#replacement").val());
+    var new_sentence = findReplace(sentence, word, replacement);
+
+    $(".new_sentence").text(new_sentence);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
