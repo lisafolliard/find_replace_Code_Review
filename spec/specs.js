893293da03ea5b490d2c5ec1b1a38d2003005676
Lisa Folliard <lisaf37@gmail.com>
2015-09-11 12:35:10 -0700

    find all instances of a word in a sentence and replace with a new word

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe('findReplace', function() {
-  it('find a word in a sentence and replace with another word', function() {
+  it('find a word in a sentence and replace with a new word', function() {
     expect(findReplace("eat more candy", "candy", "vegetables")).to.equal("eat more vegetables");
+  });
+
+  it('find all instances of a word in a sentence and replace with a new word', function() {
+    expect(findReplace("cake cake sugar", "cake", "lemon")).to.equal("lemon lemon sugar");
   });
 });
